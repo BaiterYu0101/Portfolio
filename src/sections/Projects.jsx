fde1615e7fef82bced875ff023820be74668221a
Wong Guang Yu <guangyuwenrou@gmail.com>
2026-03-30 00:24:41 +0800

    Final Check for base code

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useMediaQuery } from 'react-responsive';
 import Project from "../components/Project";
 import { myProjects } from "../constants";
 import { motion, useMotionValue, useSpring } from "motion/react";
@@ -12,18 +13,19 @@ const Projects = () => {
     y.set(e.clientY + 20);
   };
   const [preview, setPreview] = useState(null);
+  const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
     <section
       id="work"
-      onMouseMove={handleMouseMove}
+      onMouseMove={isMobile ? undefined : handleMouseMove}
       className="relative c-space section-spacing"
     >
       <h2 className="text-heading">My Selected Projects</h2>
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full" />
       {myProjects.map((project) => (
-        <Project key={project.id} {...project} setPreview={setPreview} />
+        <Project key={project.id} {...project} setPreview={isMobile ? undefined : setPreview} />
       ))}
-      {preview && (
+      {!isMobile && preview && (
         <motion.img
           className="fixed top-0 left-0 z-50 object-cover h-56 rounded-lg shadow-lg pointer-events-none w-80"
           src={preview}
