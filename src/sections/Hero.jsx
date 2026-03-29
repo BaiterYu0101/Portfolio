d6ff8cdfe0eaf503449c562f3530dd6681b5bcbf
Wong Guang Yu <guangyuwenrou@gmail.com>
2026-03-29 21:33:14 +0800

    refactor: enhance Hero component with mobile responsiveness and clean up Canvas props

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,10 +2,13 @@ import HeroText from "../components/HeroText";
 import { Suspense } from "react";
 import ParallaxBackground from "../components/ParallaxBackground";
 import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera, Environment } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Environment } from "@react-three/drei";
 import { Drone } from "../components/Drone";
 
+import { useMediaQuery } from 'react-responsive';
+
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
     <section className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space">
       <HeroText />
@@ -14,7 +17,7 @@ const Hero = () => {
         className="absolute inset-0"
         style={{ width: "100vw", height: "100vh" }}
       >
-  <Canvas style={{ pointerEvents: "none" }}>
+  <Canvas>
           <PerspectiveCamera makeDefault position={[0, 0, 5]} fov={50} />
           <ambientLight intensity={1.2} />
           <directionalLight position={[5, 10, 5]} intensity={1.2} castShadow />
