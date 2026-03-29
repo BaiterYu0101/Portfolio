c2ed3dfcc886e8c930ff379ee127d701c6313f18
Wong Guang Yu <guangyuwenrou@gmail.com>
2026-03-29 21:13:50 +0800

    refactor: simplify Hero component and add drone rotation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,11 @@
 import HeroText from "../components/HeroText";
-import { useMediaQuery } from 'react-responsive';
 import { Suspense } from "react";
 import ParallaxBackground from "../components/ParallaxBackground";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, PerspectiveCamera, Environment } from "@react-three/drei";
+import { PerspectiveCamera, Environment } from "@react-three/drei";
 import { Drone } from "../components/Drone";
 
 const Hero = () => {
-  const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
     <section className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space">
       <HeroText />
@@ -16,20 +14,13 @@ const Hero = () => {
         className="absolute inset-0"
         style={{ width: "100vw", height: "100vh" }}
       >
-        <Canvas>
+  <Canvas style={{ pointerEvents: "none" }}>
           <PerspectiveCamera makeDefault position={[0, 0, 5]} fov={50} />
           <ambientLight intensity={1.2} />
           <directionalLight position={[5, 10, 5]} intensity={1.2} castShadow />
           <pointLight position={[-5, 5, 5]} intensity={0.8} color="#ff00ff" />
           <pointLight position={[5, -5, 5]} intensity={0.6} color="#00ffff" />
           <Environment preset="sunset" />
-          <OrbitControls 
-            enableZoom={false}
-            autoRotate
-            autoRotateSpeed={2}
-            enablePan={false}
-            enableRotate={!isMobile}
-          />
           <Suspense fallback={null}>
             <Drone />
           </Suspense>
